006a94fd1606b8c8c6bc0175079d1779e33cfe1e
sginko <s.ginkota@gmail.com>
2024-07-15 21:13:55 +0200

    chages

--- src/main/java/pl/sginko/travelexpense/configurations/Configurations.java
+++ src/main/java/pl/sginko/travelexpense/configurations/Configurations.java
@@ -1,19 +1,10 @@
 package pl.sginko.travelexpense.configurations;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 class Configurations {
