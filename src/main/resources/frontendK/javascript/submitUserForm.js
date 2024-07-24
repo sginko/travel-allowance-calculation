98ca4cfb0481ef9d444c7a81b20ee90be5d59f73
sginko <s.ginkota@gmail.com>
2024-07-24 08:33:49 +0200

    some changes

--- src/main/resources/frontendK/javascript/submitUserForm.js
+++ src/main/resources/frontendK/javascript/submitUserForm.js
@@ -1,12 +1,12 @@
-document.getElementById('register-user-form').addEventListener('submit', submitForm);
+const form = document.getElementById('register-user-form');
+form.addEventListener('submit', submitForm);
 
 async function submitForm(event) {
     event.preventDefault();
-    clearErrors();
-    const form = document.getElementById('register-user-form');
     const formData = new FormData(form);
     const jsonData = JSON.stringify(Object.fromEntries(formData.entries()));
-
+    console.log(jsonData);
+    // displayMessageBox("user-message-box");
     try {
         const response = await fetch('http://localhost:8080/api/v1/users', {
             method: 'POST',
@@ -17,34 +17,13 @@ async function submitForm(event) {
         });
 
         if (!response.ok) {
-            const errorText = await response.text();
-            displayErrors(errorText);
-            throw new Error('Network response was not ok');
+            const errorData = await response.json();
+            displayErrorBox("user-error-box", errorData);
+        } else {
+            displayMessageBox("user-message-box");
         }
 
-        console.log('Pracownik został pomyślnie dodany');
-        alert('User został pomyślnie dodany');
     } catch (error) {
-        console.error('Błąd:', error);
-        alert('Wystąpił błąd podczas dodawania usera');
+        displayErrorBox("user-error-box", error);
     }
-}
-
-function displayErrors(errors) {
-    const errorList = errors.split('\n');
-    errorList.forEach(error => {
-        const [field, message] = error.split(': ');
-        const inputField = document.querySelector(`[name=${field}]`);
-        if (inputField) {
-            const errorMessageElement = document.createElement('div');
-            errorMessageElement.classList.add('error-message');
-            errorMessageElement.innerText = message;
-            inputField.parentNode.appendChild(errorMessageElement);
-        }
-    });
-}
-
-function clearErrors() {
-    const errorMessages = document.querySelectorAll('.error-message');
-    errorMessages.forEach(errorMessage => errorMessage.remove());
 }
